5729ab2a4c78f16060c18e551c24c12fe2f1d9dc
Nikki Gupta <niharikg@uci.edu>
2018-03-10 14:44:29 -0800

    updated database

--- database/nndb.sql
+++ database/nndb.sql
@@ -16,13 +16,14 @@ create table `posting` (
     `zipcode` int not null,
     `foodType` varchar(50) not null,
     `foodName` varchar(100) not null,
+    `description` varchar(200) default null,
     `servings` int not null,
     `interested` int not null default 0,
     primary key(`pid`)
 );
 
-insert into `posting`(`endTime`,`organization`,`street`,`city`,`state`,`zipcode`,`foodType`,`foodName`,`servings`,`interested`)
+insert into `posting`(`endTime`,`organization`,`street`,`city`,`state`,`zipcode`,`foodType`,`foodName`,`description`,`servings`,`interested`)
 values
-	('22:00:00','Subway','123 Main St','Irvine','California','92617','Deli','Bread','500',4),
-    ('23:30:00','WomenWhoCode','808 J Street','San Diego','California','92101','Mediterranean','Hummus','350',10)
-    ('21:45:00','Lotus Thai Cuisine', '906 Market St','San Diego','California','92101','Thai', 'Pad Thai','145',2);
+	('22:00:00','Subway','123 Main St','Irvine','California','92617','Deli','Bread','250 Italian, 250 9-grain wheat','500',4),
+    ('23:30:00','WomenWhoCode','808 J Street','San Diego','California','92101','Mediterranean','Hummus',null,'350',10),
+    ('21:45:00','Lotus Thai Cuisine', '906 Market St','San Diego','California','92101','Thai', 'Pad Thai','Chicken pad thai with peanuts and mixed vegetables','145',2);
